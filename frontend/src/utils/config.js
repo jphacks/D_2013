f5535f432b3ff57cb05df6bc650243e03163d296
Watayo <ryo0616mani@gmail.com>
2020-11-03 04:37:12 +0900

    feature/auth

---
+++ frontend/src/utils/config.js
@@ -0,0 +1,10 @@
+export let config = {
+  apiKey: "AIzaSyDht_xN58eNK3e624iiPbbJqUn_r2Xr41o",
+  authDomain: "sampleauthwithfacebook.firebaseapp.com",
+  databaseURL: "https://sampleauthwithfacebook.firebaseio.com",
+  projectId: "sampleauthwithfacebook",
+  storageBucket: "sampleauthwithfacebook.appspot.com",
+  messagingSenderId: "843483616334", //
+  appId: "1:843483616334:web:182a478c05aeee3b87df1f",
+  measurementId: "G-DD5PWJ9SLG"
+}
