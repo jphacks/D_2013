d1eb5d600c342336cda5fd87794ac365a5d78f14
廣瀬健二朗 <hiroken506@gmail.com>
2020-11-03 16:56:40 +0900

    コア機能の実装

---
+++ unity/JPHacks2020/Assets/Scripts/SubGameB/MoleAppearScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mole
+{
+    public class MoleAppearScript : MonoBehaviour
+    {
+        [SerializeField] GameObject[] moles;
+        
+
+        private int _apperPos;
+        private float _interval;
+
+        private float _timer = 0;
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _interval = 2f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _timer += Time.deltaTime;
+
+          
+
+            if (_timer >= _interval) {
+
+                _apperPos = Random.Range(0,9);
+                moles[_apperPos].SetActive(true);
+                StartCoroutine("disapp", _apperPos);
+
+                _timer = 0;
+            }
+            
+        }
+        
+        private IEnumerator disapp(int disPos) {
+            yield return new WaitForSeconds(5f);
+            moles[disPos].SetActive(false);
+        }
+    }
+
+}
---
+++ unity/JPHacks2020/Assets/Scripts/SubGameB/ScoreScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mole
+{
+    public class ScoreScript : MonoBehaviour
+    {
+        [SerializeField] Button[] moleButton;
+        [SerializeField] Text scoretext;
+
+        private int _score=0;
+        // Start is called before the first frame update
+        void Start()
+        {
+            scoretext.text = _score.ToString();
+            
+            moleButton[0].onClick.AddListener(() => addPoint(0));
+            moleButton[1].onClick.AddListener(() => addPoint(1));
+            moleButton[2].onClick.AddListener(() => addPoint(2));
+            moleButton[3].onClick.AddListener(() => addPoint(3));
+            moleButton[4].onClick.AddListener(() => addPoint(4));
+            moleButton[5].onClick.AddListener(() => addPoint(5));
+            moleButton[6].onClick.AddListener(() => addPoint(6));
+            moleButton[7].onClick.AddListener(() => addPoint(7));
+            moleButton[8].onClick.AddListener(() => addPoint(8));
+            
+
+           
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            scoretext.text = "スコア : " +  _score.ToString();
+        }
+
+        
+
+        private void addPoint(int num) {
+
+            
+                Debug.Log(num);
+            if (moleButton[num].transform.GetChild(0).gameObject.activeSelf) {
+                moleButton[num].transform.GetChild(0).gameObject.SetActive(false);
+                _score++;
+            }
+
+           
+        }
+    }
+}
