4d2152b3047ab74e3b33fd0e960cc11bffe41234
mittan1330 <hudabo1330@gmail.com>
2020-11-03 22:13:19 +0900

    add start,endScene

--- unity/JPHacks2020/Assets/Scripts/Maze/MazeManager.cs
+++ unity/JPHacks2020/Assets/Scripts/Maze/MazeManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Maze
 {
@@ -9,6 +10,17 @@ namespace Maze
         //trueならP1が操作
         [SerializeField] private int _playerId;
         [SerializeField] private GameObject[] _Camera;
+
+        private float _startTime = 4;
+        private float _timer = 0;
+        [SerializeField] private GameObject startView;
+        [SerializeField] private GameObject startMessage;
+        [SerializeField] private GameObject scooreView;
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button endButton;
+        public bool isPlay = false;
+        private bool _isTimerCount = true;
+        public bool isClear = false;
 
         void OnEnable()
         {
@@ -23,12 +35,42 @@ namespace Maze
                 _Camera[i].SetActive(false);
             }
             _Camera[_playerId - 1].SetActive(true);
+            startButton.onClick.AddListener(UiFalse);
+            endButton.onClick.AddListener(ScoreUi);
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (_timer >= _startTime - 1)
+            {
+                startView.SetActive(false);
+                if (_timer >= _startTime)
+                {
+                    startMessage.SetActive(false);
+                    isPlay = true;
+                    Debug.Log("START");
+                    _isTimerCount = false;
+                    _timer = 0;
+                }
+            }
+            if(_isTimerCount)_timer += Time.deltaTime;
+            if (isClear)
+            {
+                Debug.Log("CLEAR");
+                scooreView.SetActive(true);
+                isPlay = false;
+                isClear = false;
+            }
+        }
 
+        void UiFalse()
+        {
+            startView.SetActive(false);
+        }
+        void ScoreUi()
+        {
+            Debug.Log("HOGEHOGE");
         }
     }
 }
