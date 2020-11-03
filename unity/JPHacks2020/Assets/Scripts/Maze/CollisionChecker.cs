ab91b56ec022f3f731e403d42408022f9a37edc9
mittan1330 <hudabo1330@gmail.com>
2020-11-03 11:09:46 +0900

    Add Controller

---
+++ unity/JPHacks2020/Assets/Scripts/Maze/CollisionChecker.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maze
+{
+    public class CollisionChecker : MonoBehaviour
+    {
+        [SerializeField] bool _isPlayer;
+        [SerializeField] int _collisionNum;
+        [SerializeField] MazePlayer1 mazePlayer1;
+        [SerializeField] MazePlayer2 mazePlayer2;
+
+   
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Wall")
+            {
+                switch (_collisionNum)
+                {
+                    case 1:
+                        if (_isPlayer == true)
+                        {
+                            mazePlayer1.collisionFront = true;
+                        }
+                        else
+                        {
+                            mazePlayer2.collisionFront = true;
+                        }
+                        break;
+                    case 2:
+                        if (_isPlayer == true)
+                        {
+                            mazePlayer1.collisionBack = true;
+                        }
+                        else
+                        {
+                            mazePlayer2.collisionBack = true;
+                        }
+                        break;
+                    case 3:
+                        if (_isPlayer == true)
+                        {
+                            mazePlayer1.collisionRight = true;
+                        }
+                        else
+                        {
+                            mazePlayer2.collisionRight = true;
+                        }
+                        break;
+                    case 4:
+                        if (_isPlayer == true)
+                        {
+                            mazePlayer1.collisionLeft = true;
+                        }
+                        else
+                        {
+                            mazePlayer2.collisionLeft = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            switch (_collisionNum)
+            {
+                case 1:
+                    if (_isPlayer == true)
+                    {
+                        mazePlayer1.collisionFront = false;
+                    }
+                    else
+                    {
+                        mazePlayer2.collisionFront = false;
+                    }
+                    break;
+                case 2:
+                    if (_isPlayer == true)
+                    {
+                        mazePlayer1.collisionBack = false;
+                    }
+                    else
+                    {
+                        mazePlayer2.collisionBack = false;
+                    }
+                    break;
+                case 3:
+                    if (_isPlayer == true)
+                    {
+                        mazePlayer1.collisionRight = false;
+                    }
+                    else
+                    {
+                        mazePlayer2.collisionRight = false;
+                    }
+                    break;
+                case 4:
+                    if (_isPlayer == true)
+                    {
+                        mazePlayer1.collisionLeft = false;
+                    }
+                    else
+                    {
+                        mazePlayer2.collisionLeft = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
--- unity/JPHacks2020/Assets/Scripts/Maze/MazeManager.cs
+++ unity/JPHacks2020/Assets/Scripts/Maze/MazeManager.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeManager : MonoBehaviour
+namespace Maze
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MazeManager : MonoBehaviour
     {
+        //trueならP1が操作
+        [SerializeField] bool _isPlayer;
+        [SerializeField] GameObject _CameraP1;
+        [SerializeField] GameObject _CameraP2;
         
-    }
+        
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void OnEnable()
+        {
+            
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(_isPlayer == true)
+            {
+                _CameraP2.SetActive(false);
+            }
+            else
+            {
+                _CameraP1.SetActive(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
