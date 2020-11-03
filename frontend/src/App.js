a7d8a5d4bdbffc2d602e3c0ecc5768025f4e7f4d
Shunya078 <Shunya078@users.noreply.github.com>
2020-11-03 18:44:31 +0000

    Format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import UnityView from '@asmadsen/react-native-unity-view';
+import UnityView from "@asmadsen/react-native-unity-view";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello JPHacks2020!</Text>
-      <UnityView style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, }} />
+      <UnityView
+        style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}
+      />
     </View>
   );
 }
