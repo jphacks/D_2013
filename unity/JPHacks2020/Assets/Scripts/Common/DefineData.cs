9644969d8e1ad519ad8b79db9106cfbe26628b76
fukuchang <aki-basketplayer21.eye@i.softbank.jp>
2020-11-03 14:24:24 +0900

    Add Definition Data current status

---
+++ unity/JPHacks2020/Assets/Scripts/Common/DefineData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Common
+{
+    public static class DefineData
+    {
+        public enum SCENE_NAME {
+            TITLE,
+            REGISTRATION,
+            SERVICE_SETTING,
+            GAMELIST,
+            RESULT,
+            GAME_A,
+            GAME_B,
+            GAME_C,
+            GAME_D
+        };
+
+        public static void LoadScene(SCENE_NAME scene)
+        {
+            SceneManager.LoadScene(transSceneName[scene]);
+        }
+
+        // enumをScene名に変換
+        private static Dictionary<SCENE_NAME, string> transSceneName = new Dictionary<SCENE_NAME, string>() {
+            { SCENE_NAME.TITLE, "Title"},
+            { SCENE_NAME.REGISTRATION, "Registration"},
+            { SCENE_NAME.SERVICE_SETTING, "ServiceSetting"},
+            { SCENE_NAME.GAMELIST, "GameList"},
+            { SCENE_NAME.RESULT, "Result"},
+            { SCENE_NAME.GAME_A, "SubGameA"},
+            { SCENE_NAME.GAME_B, "SubGameA"},
+            { SCENE_NAME.GAME_C, "SubGameA"},
+            { SCENE_NAME.GAME_D, "SubGameA"},
+        };
+
+
+    }
+
+}
---
+++ unity/JPHacks2020/Assets/Scripts/Common/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Common
+{
+    public class GameManager : SingletonMonoBehaviour<GameManager>
+    {
+        private string _userName;
+        private DateTime _sleepTime;
+        private DateTime _awakeTime;
+    }
+}
+
