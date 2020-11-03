e32081825f68a756debaa26916ada48faa42a882
Shunya078 <kikukan007@gmail.com>
2020-11-04 03:43:54 +0900

    re:add xcode data

---
+++ frontend/src/ios/JPHacks2020/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JPHacks2020
+//
+//  Created by OtsuboShunya on 2020/11/04.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
