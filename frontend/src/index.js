961d3e59ff5bab2d491a7db6521ebdb6e475e6fb
Shunya078 <kikukan007@gmail.com>
2020-11-05 00:46:14 +0900

    fix index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,3 +3,24 @@ import { ApplicationProvider } from "@ui-kitten/components";
 import { registerRootComponent } from "expo";
 import React from "react";
 import App from "src/App";
+
+import firebase from "firebase";
+import { FireclientProvider } from "react-fireclient";
+import { AuthProvider } from "src/utils/auth";
+import { config } from "src/utils/config.js";
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const Root = () => (
+  <FireclientProvider firestoreDB={db}>
+    <AuthProvider auth={auth}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <App />
+      </ApplicationProvider>
+    </AuthProvider>
+  </FireclientProvider>
+);
+
+registerRootComponent(Root);
