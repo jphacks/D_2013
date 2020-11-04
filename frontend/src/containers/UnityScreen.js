f4120af78ee868f3aaede412690d0ae1fe82fd2c
Shunya078 <kikukan007@gmail.com>
2020-11-05 00:57:09 +0900

    re add UnityScreen container

--- frontend/src/containers/UnityScreen.js
+++ frontend/src/containers/UnityScreen.js
@@ -5,7 +5,21 @@ import WithHeader from "src/components/WithHeader";
 const UnityScreen = () => {
   const [errorMsg, setErrorMsg] = useState(null);
 
-  return <>{errorMsg !== null && <Text>{errorMsg}</Text>}</>;
+  return (
+    <>
+      {errorMsg !== null && <Text>{errorMsg}</Text>}
+      <View style={Styles.container}>
+        <Text>Hello JPHacks2020!</Text>
+        <WebView
+          originWhitelist={["*"]}
+          // ここに使用URLを流す
+          source={require("http://google.com")}
+          style={{ marginTop: 50, marginBottom: 50 }}
+        />
+        <Text>Hello JPHacks2020</Text>
+      </View>
+    </>
+  );
 };
 
 export default WithHeader(UnityScreen, "Unity");
