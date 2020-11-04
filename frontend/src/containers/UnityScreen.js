98d88719322fa29221158afda36a29a5e507fdf4
Shunya078 <kikukan007@gmail.com>
2020-11-05 00:45:41 +0900

    add container base data

---
+++ frontend/src/containers/UnityScreen.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+import WithHeader from "src/components/WithHeader";
+
+const UnityScreen = () => {
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  return (
+    <>
+      
+    </>
+  )
+};
+
+export default WithHeader(UnityScreen, "Unity");
