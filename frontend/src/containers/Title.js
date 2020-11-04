51e399ca38cedb640fdd8d86c56c5b5455199b2e
Shunya078 <Shunya078@users.noreply.github.com>
2020-11-04 15:57:55 +0000

    Format

--- frontend/src/containers/Title.js
+++ frontend/src/containers/Title.js
@@ -5,12 +5,7 @@ import WithHeader from "src/components/WithHeader";
 const UnityScreen = () => {
   const [errorMsg, setErrorMsg] = useState(null);
 
-  return (
-    <>
-      {errorMsg !== null && <Text>{errorMsg}</Text>}
-      
-    </>
-  );
+  return <>{errorMsg !== null && <Text>{errorMsg}</Text>}</>;
 };
 
 export default WithHeader(UnityScreen, "Unity");
