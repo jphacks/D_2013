da3f3cfe4002248e2a0a6dda7f840b4a3060eb7f
fukuchang <aki-basketplayer21.eye@i.softbank.jp>
2020-11-04 19:09:48 +0900

    Add make json parser

---
+++ unity/JPHacks2020/Assets/Scripts/Common/UserData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Common
+{
+    [Serializable]
+    public class UserData
+    {
+        public string userName;
+        public bool isClear;
+        public DefineData.SCENE_NAME sceneName;
+    }
+}
