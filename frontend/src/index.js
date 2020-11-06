da72f46f8fa62316fe6caeb06018f60515b8fc0f
Shunya078 <kikukan007@gmail.com>
2020-11-07 03:14:44 +0900

    fix routing & route in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,23 @@ import { registerRootComponent } from "expo";
 import React from "react";
 import App from "src/App";
 
+import firebase from "firebase";
 import { FireclientProvider } from "react-fireclient";
+import { AuthProvider } from "src/utils/auth";
+import { config } from "src/utils/config";
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 const Root = () => (
   <FireclientProvider firestoreDB={db}>
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <App />
-    </ApplicationProvider>
-  </FireclientProvider>
+      <AuthProvider auth={auth}>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <App />
+        </ApplicationProvider>
+      </AuthProvider>
+    </FireclientProvider>
 );
 
 registerRootComponent(Root);
